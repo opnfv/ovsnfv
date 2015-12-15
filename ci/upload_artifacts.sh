21683d159c8cfee80dcb100bb02c0f24dcef276d
Thomas F Herbert <therbert@redhat.com>
2015-12-15 16:09:36 -0500

    Variable name fixed. OVS build works. Uploads RPM artifacts to artifactory.
    Signed-off-by: Thomas F Herbert <therbert@redhat.com>

--- ci/upload_artifacts.sh
+++ ci/upload_artifacts.sh
@@ -12,11 +12,11 @@ export RPMFILE_D=openvswitch-debuginfo-$VERSION-1.x86_64.rpm
 export RPMFILE1=openvswitch-$VERSION-1.x86_64.rpm
 
 # upload artifact and additional files to google storage
-echo gsutil cp $TMP_RELEASE_DIR/$RPMFILE_D gs://$GS_URL/opnfv-$RPMFILE_D > gsutil.iso.log 2>&1
-gsutil cp $TMP_RELEASE_DIR/$RPMFILE_D gs://$GS_URL/opnfv-$RPMFILE_D > gsutil.iso.log 2>&1
+echo gsutil cp $TMP_RELEASE_DIR/$RPMFILE_D gs://$GS_URL/opnfv-$RPMFILE_D
+gsutil cp $TMP_RELEASE_DIR/$RPMFILE_D gs://$GS_URL/opnfv-$RPMFILE_D
 
-echo gsutil cp $TMP_RELEASE_DIR/$RPMFILE1 gs://$GS_URL/opnfv-$RPMFILE1 > gsutil.iso.log 2>&1
-gsutil cp $TMP_RELEASE_DIR/$RPMFILE1 gs://$GS_URL/opnfv-$RPMFILE1 > gsutil.iso.log 2>&1
+echo gsutil cp $TMP_RELEASE_DIR/$RPMFILE1 gs://$GS_URL/opnfv-$RPMFILE1
+gsutil cp $TMP_RELEASE_DIR/$RPMFILE1 gs://$GS_URL/opnfv-$RPMFILE1
 
 #
 #if [[ ! "$JOB_NAME" =~ (verify|merge) ]]; then
@@ -28,4 +28,4 @@ gsutil cp $TMP_RELEASE_DIR/$RPMFILE1 gs://$GS_URL/opnfv-$RPMFILE1 > gsutil.iso.l
 echo
 echo "------------------------------------------------------"
 echo "Done!"
-echo "Artifact is available as http://$GS_URL/opnfv-$RPMFILE"
+echo "Artifact is available as http://$GS_URL/opnfv-$RPMFILE1"
