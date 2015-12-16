e4d647084bacd4b5228deb41d6b638a3efef2fe3
Thomas F Herbert <therbert@redhat.com>
2015-12-16 14:44:53 -0500

    Fixes based on reviewer's comments

--- ci/upload_artifacts.sh
+++ ci/upload_artifacts.sh
@@ -9,23 +9,16 @@ echo "-----------------------------------------"
 echo
 
 export RPMFILE_D=openvswitch-debuginfo-$VERSION-1.x86_64.rpm
-export RPMFILE1=openvswitch-$VERSION-1.x86_64.rpm
+export RPMFILE=openvswitch-$VERSION-1.x86_64.rpm
 
 # upload artifact and additional files to google storage
 echo gsutil cp $TMP_RELEASE_DIR/$RPMFILE_D gs://$GS_URL/opnfv-$RPMFILE_D
 gsutil cp $TMP_RELEASE_DIR/$RPMFILE_D gs://$GS_URL/opnfv-$RPMFILE_D
 
-echo gsutil cp $TMP_RELEASE_DIR/$RPMFILE1 gs://$GS_URL/opnfv-$RPMFILE1
-gsutil cp $TMP_RELEASE_DIR/$RPMFILE1 gs://$GS_URL/opnfv-$RPMFILE1
-
-#
-#if [[ ! "$JOB_NAME" =~ (verify|merge) ]]; then
-#    gsutil cp $WORKSPACE/opnfv.properties gs://$GS_URL/latest.properties > gsutil.latest.log 2>&1
-#elif [[ "$JOB_NAME" =~ "merge" ]]; then
-#    echo "Uploaded Fuel ISO for a merged change"
-#fi
+echo gsutil cp $TMP_RELEASE_DIR/$RPMFILE gs://$GS_URL/opnfv-$RPMFILE
+gsutil cp $TMP_RELEASE_DIR/$RPMFILE gs://$GS_URL/opnfv-$RPMFILE
 
 echo
 echo "------------------------------------------------------"
 echo "Done!"
-echo "Artifact is available as http://$GS_URL/opnfv-$RPMFILE1"
+echo "Artifacts are available as http://$GS_URL/opnfv-$RPMFILE1"
