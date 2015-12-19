2b290fdf797a256dbcac2510424182c4b9240b10
Thomas F Herbert <therbert@redhat.com>
2015-12-19 16:12:07 -0500

    Use GS_URL as it is now defined by Jenkins and detect Jenkins job name
    Fix type
    Fix daily JOB_NAME check
    Fix daily substring check
    
    Change-Id: If5c4fd472318555d885076a62340bd7ec4e9d2a7
    Signed-off-by: Thomas F Herbert <therbert@redhat.com>

--- ci/upload_artifacts.sh
+++ ci/upload_artifacts.sh
@@ -12,13 +12,13 @@ export RPMFILE_D=openvswitch-debuginfo-$VERSION-1.x86_64.rpm
 export RPMFILE=openvswitch-$VERSION-1.x86_64.rpm
 
 # upload artifact and additional files to google storage
-echo gsutil cp $TMP_RELEASE_DIR/$RPMFILE_D gs://artifacts.opnfv.org/ovsnfv/opnfv-$DATE-$RPMFILE_D
-gsutil cp $TMP_RELEASE_DIR/$RPMFILE_D gs://artifacts.opnfv.org/ovsnfv/opnfv-$DATE-$RPMFILE_D
+echo gsutil cp $TMP_RELEASE_DIR/$RPMFILE_D $GS_URL/opnfv-$DATE-$RPMFILE_D
+gsutil cp $TMP_RELEASE_DIR/$RPMFILE_D $GS_URL/opnfv-$DATE-$RPMFILE_D
 
-echo gsutil cp $TMP_RELEASE_DIR/$RPMFILE gs://artifacts.opnfv.org/ovsnfv/opnfv-$DATE-$RPMFILE
-gsutil cp $TMP_RELEASE_DIR/$RPMFILE gs://artifacts.opnfv.org/ovsnfv/opnfv-$DATE-$RPMFILE
+echo gsutil cp $TMP_RELEASE_DIR/$RPMFILE $GS_URL/opnfv-$DATE-$RPMFILE
+gsutil cp $TMP_RELEASE_DIR/$RPMFILE $GS_URL/opnfv-$DATE-$RPMFILE
 
 echo
 echo "------------------------------------------------------"
 echo "Done!"
-echo "Artifacts are available as http://artifacts.opnfv.org/ovsnfv/opnfv-$DATE-$RPMFILE"
+echo "Artifacts are available as $GS_URL/opnfv-$DATE-$RPMFILE"
