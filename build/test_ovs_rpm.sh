defa054512484ac5e269d4ece83c6e9a10cb5231
Thomas F Herbert <therbert@redhat.com>
2016-01-23 19:06:55 -0500

    Introduce ability to accept command arguments to build with DPDK or Linux kernel. Specify patches to apply, build kernel module RPM and build with a special version of the kernel.
    Also instack_ovs.sh creates a VM for building and initial testing of RPMs and
    copies scripts to VMs
    
    build_ovs_rpm.sh builds the RPM according to command line options.
    test_ovs_rpm.sh tests the RPM by installing the RPM and running some ovs
    commands to create a bridge and dump flows.
    
    Also add two scripts generally to be executed in VM to do actual building and
    Testing.
    
    For command line options execute build/buildtestovs.sh -h
    
    Change-Id: I415ca39afab27482b1cb473d392f48b36c8e0745
    Signed-off-by: Thomas F Herbert <therbert@redhat.com>

---
+++ build/test_ovs_rpm.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+##############################################################################
+# Copyright (c) 2016 Red Hat Inc. and others.
+# therbert@redhat.com
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+set -e
+declare -i CNT
+
+echo "==============================="
+echo executing $0 $@
+echo executing on machine `uname -a`
+
+
+usage() {
+    echo run buildtestovs.sh -h for help
+}
+
+while getopts "dg:hp:u:v" opt; do
+    case "$opt" in
+        d)
+            DPDK="yes"
+            ;;
+        g)
+            TAG=${OPTARG}
+            ;;
+        h|\?)
+            usage
+            exit 1
+            ;;
+        k)
+            kmod="yes"
+            ;;
+        u)
+            OVS_REPO_URL=${OPTARG}
+            ;;
+        v)
+            verbose="yes"
+            ;;
+    esac
+done
+
+HOME=`pwd`
+TOPDIR=$HOME
+TMPDIR=$TOPDIR/ovsrpm
+
+if [ -d $TMPDIR ]
+then
+    rm -rf $TMPDIR
+fi
+
+mkdir -p $TMPDIR
+
+cd $TMPDIR
+
+mkdir -p $HOME/rpmbuild/RPMS
+mkdir -p $HOME/rpmbuild/SOURCES
+mkdir -p $HOME/rpmbuild/SPECS
+mkdir -p $HOME/rpmbuild/SRPMS
+
+RPMDIR=$HOME/rpmbuild
+
+echo " Testing installation of kmod RPM"
+if [ ! -z $kmod ]; then
+    echo "Install kernel module"
+    sudo rpm -ivh $RPMDIR/kmod*.rpm
+    echo " Kernel RPM installed."
+fi
+echo "Testing User Space RPM"
+sudo rpm -ivh $RPMDIR/RPMS/x86_64/*.rpm
+
+sudo service openvswitch start
+
+sudo ovs-vsctl show
+sudo ovs-vsctl add-br br1
+sudo ovs-ofctl dump-flows br1
+
+exit 0
