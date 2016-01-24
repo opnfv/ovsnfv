3513ab0a0c41218585e0c3a7e49fb58e7368f8e2
Thomas F Herbert <therbert@redhat.com>
2016-01-23 19:06:55 -0500

    Changes build scripts to introduce ability to accept command arguments to build either with DPDK data path or with Linux kernel. Supports build with patches from patch URL, build kernel module RPM and required special version of the kernel.
    instack_ovs.sh creates a VM for building and initial testing of RPMs and
    copies scripts to the VM.
    
    Also add two scripts generally to be executed in VM to do actual building and
    Testing.
    
    build_ovs_rpm.sh builds the RPM according to command line options.
    test_ovs_rpm.sh tests the RPM by installing the RPM and running some ovs
    commands to create a bridge and dump flows.
    
    For command line options execute build/buildtestovs.sh -h
    
    Currently set in config file with defaults to build OVS RPM from master.
    Building OVS kernel module RPM currently not functional because of upstream bug.
    Building OVS RPM with DPDK data path is TBD at this point.
    
    Change-Id: I415ca39afab27482b1cb473d392f48b36c8e0745
    Signed-off-by: Thomas F Herbert <therbert@redhat.com>

--- ci/buildovs.sh
+++ ci/buildovs.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 ##############################################################################
-# Copyright (c) 2015 Red Hat Inc. and others.
+# Copyright (c) 2015,2016 Red Hat Inc. and others.
 # therbert@redhat.com
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Apache License, Version 2.0
 # which accompanies this distribution, and is available at
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
+set -e
+
+echo "==================================="
+echo executing $0 $@
 
 # Check to verify that I am being run by Jenkins CI.
 
@@ -16,9 +20,7 @@ if [ -z ${WORKSPACE+1} ]; then
 fi
 
 
-if [ -z ${OVSTAG+1} ]; then
-    export TAG=master
-else
+if [ ${OVSTAG} ]; then
     export TAG=$OVSTAG
 fi
 
@@ -38,7 +40,6 @@ if [ ! -f $BUILD_BASE/config ]; then
 fi
 
 export PATH=$PATH:$WORKSPACE/ci:$BUILD_BASE
-
 source $BUILD_BASE/config
 
 cd $BUILD_BASE
@@ -65,95 +66,26 @@ then
     mkdir -p $TMP_RELEASE_DIR
 fi
 
-# Centos build server should support the following build prerequisites
-
-# yum install gcc make python-devel openssl-devel kernel-devel graphviz \
-# kernel-debug-devel autoconf automake rpm-build redhat-rpm-config \
-# libtool
-
-if [ -d $TMPDIR ]
-then
-    rm -rf $TMPDIR
-fi
-
-mkdir $TMPDIR
-
-cd $TMPDIR
-
-echo "---------------------"
-echo "Clone git repo $TAG"
-echo
-git clone https://github.com/openvswitch/ovs.git
-
-cd ovs
-echo "--------------------"
-echo "Checkout OVS $TAG"
-echo
-if [[ ! "$TAG" =~ "master" ]]; then
-    git checkout $TAG
-fi
-./boot.sh
-./configure
-echo "--------------------"
-echo "Make OVS $TAG"
-echo
-make
 #
-# Get version for master
+# Build ovs rpm without DPDK from ovs master
 #
-echo "--------------------"
-echo "Get OVS version for $TAG"
-echo
-if [[ "$TAG" =~ "master" ]]; then
-    v=$($TMPDIR/ovs/utilities/ovs-vsctl --version | head -1 | cut -d' ' -f4)
-    export VERSION=$v
-else
-    export VERSION=${TAG:1}
-fi
-
-echo "--------------------"
-echo "OVS version is $VERSION"
-echo
-echo "--------------------"
-echo "Make OVS distribution $TAG"
-echo
-
-make dist
-
-cd $TMPDIR/ovs
-
-cp openvswitch-$VERSION.tar.gz $TOPDIR/rpmbuild/SOURCES
-cp openvswitch-$VERSION.tar.gz $TMPDIR
-
-cd $TMPDIR
-tar -xzf openvswitch-$VERSION.tar.gz
-
-cd $TMPDIR/openvswitch-$VERSION
-
-
-echo "--------------------"
-echo "Build OVS RPM"
-echo
-
-if [ ! -z ${NOCHECK+1} ]; then
-    # Build RPM without checks
-    #
-    rpmbuild -bb --define "_topdir `echo $RPMDIR`" --without check rhel/openvswitch.spec
-else
-    rpmbuild -bb --define "_topdir `echo $RPMDIR`" rhel/openvswitch.spec
-fi
+echo =======Build ovs rpm and ovs kmod rpm without DPDK Test in VM==========
+    buildtestovs.sh -p none -t
+#
+# Build ovs rpm with DPDK
+#
+echo =======Build ovs rpm with DPDK Test in VM==========
+buildtestovs.sh -d -p none -t
+#
+# Build special version of vs with patches --TODO
+#
 
 # Once build is done copy product to artifactory.
+# and cleanup
+
 
 echo "---------------------------------------"
-echo "Copy RPM into $TMP_RELEASE_DIR"
-echo
-cp $RPMDIR/RPMS/x86_64/*.rpm $TMP_RELEASE_DIR
-
-# cleanup
-
-echo "---------------------------------------"
-echo "Cleanup $TMP_RELEASE_DIR"
+echo "Cleanup temporary dirs"
 echo
 cd $BUILDDIR
 
@@ -168,5 +100,18 @@ fi
 if [[ "$JOB_NAME" =~ "daily" ]]; then
     upload_artifacts.sh
 fi
+
+if [ -d $TMP_RELEASE_DIR ]; then
+    rm -rf $CACHE_RELEASE_DIR
+fi
+
+if [ -d $RPMDIR ]; then
+    rm -rf $RPMDIR
+fi
+
+# Destroy VM if one has been deployed. Also remove any local installation of
+# DPDK and OVS
+#
+clean.sh
 
 exit 0
