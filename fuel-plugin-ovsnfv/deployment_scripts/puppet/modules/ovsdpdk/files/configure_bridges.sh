052d1fb391b4e47232c0e6dc6f6aabe1a101fbf6
Michal Ptacek <michalx.ptacek@intel.com>
2016-01-28 19:23:18 +0000

    Improve linux_net.py patch
    Instead of hard coded file, sed parsing script introduced
    
    Change-Id: I1a8a9715c35b6a244fbd504cd0bd39600ccca726
    Signed-off-by: Michal Ptacek <michalx.ptacek@intel.com>

--- fuel-plugin-ovsnfv/deployment_scripts/puppet/modules/ovsdpdk/files/configure_bridges.sh
+++ fuel-plugin-ovsnfv/deployment_scripts/puppet/modules/ovsdpdk/files/configure_bridges.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-for bridge in `sudo ovs-vsctl list-br`; do
+for bridge in `sudo ovs-vsctl list-br`; do 
   sudo ovs-vsctl --no-wait set Bridge $bridge datapath_type=$1;
 done;
