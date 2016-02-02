f3099a30bf435f8d256ad236b93ae9cab7c1e241
Billy O'Mahony <billy.o.mahony@intel.com>
2016-02-02 11:52:52 +0000

    fuel-plugin: Add config item for NIC used by OVS-DPDK
    Change-Id: I7e8daa47eee658efd88922840fdcf21b8b38acd5
    Signed-off-by: Billy O'Mahony <billy.o.mahony@intel.com>
    Reviewed-by: Mark D. Gray <mark.d.gray@intel.com>
    Reviewed-by: Michal Ptacek <michalx.ptacek@intel.com>
    Reviewed-by: Michal Skalski <mskalski@mirantis.com>
    Reviewed-by: Jonas Bjurel <jonas.bjurel@ericsson.com>
    (cherry picked from commit 6ec3a89b379eb7c8e463d2d3f3240ec50ef15a3f)

--- fuel-plugin-ovsnfv/deployment_scripts/puppet/manifests/ovs-install-ovsnfv-compute.pp
+++ fuel-plugin-ovsnfv/deployment_scripts/puppet/manifests/ovs-install-ovsnfv-compute.pp
@@ -1,9 +1,11 @@
 $fuel_settings = parseyaml(file('/etc/astute.yaml'))
 $master_ip = $::fuel_settings['master_ip']
 
+$ovs_ifname = $::fuel_settings[fuel-plugin-ovsnfv][fuel-plugin-ovsnfv_text]
+
 if $operatingsystem == 'Ubuntu' {
   class { '::ovsdpdk':
-    ovs_bridge_mappings => 'default:ens1f1',
+    ovs_bridge_mappings => "default:$ovs_ifname",
     ovs_socket_mem      => '512,512',
     ovs_num_hugepages   => '2048',
     compute             => 'True',
