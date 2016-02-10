f338372d4d212886a9271a75316c5c1258ac2cb1
Thomas F Herbert <therbert@redhat.com>
2016-02-09 22:17:45 -0500

    Build OVS with dpdk
    Builds dpdk 2.2.0 and Open vSwitch 5.90.
    Includes Multi-Queue and recent patches.
    Open vSwitch RPM is now built with DPDK dataplane.
    Upstream copr (Fedora) spec files and patches are used for building.
    
    Also includes fixes of RPM building and command line options.
    Adds dpdk arg to RPM test script.
    
    Use "pure" COPR spec files without modifications. Fixed some function problems.
    Addressed reviewer comments. Delete local copies of spec files
    Build dpdk snapshot instead of using upstream snapshot.
    Fix clean script.
    
    "stack" VM with its default configuration doesn't support sse3 instruction set
    required by DPDK.  Include workaround to test RPM script in host instead of VM.
    
    Install prereqs for dpdk build.
    Use 'set -e' in all scripts for error termination.
    Add PyQt4 Pre-req.
    Move functions and respond to reviewers comments.
    
    Change-Id: I8d5892ff61a814271972e8d2279628d645b2831b
    Signed-off-by: Thomas F Herbert <therbert@redhat.com>

--- ci/buildovs.sh
+++ ci/buildovs.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-##############################################################################
-# Copyright (c) 2015,2016 Red Hat Inc. and others.
-# therbert@redhat.com
-# All rights reserved. This program and the accompanying materials
-# are made available under the terms of the Apache License, Version 2.0
-# which accompanies this distribution, and is available at
-# http://www.apache.org/licenses/LICENSE-2.0
-##############################################################################
+
+# Copyright (c) 2016 Open Platform for NFV Project, Inc. and its contributors
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
 set -e
 
 echo "==================================="
@@ -52,10 +59,6 @@ export CACHE_DIR=$TOPDIR/cache
 export TMPDIR=$TOPDIR/scratch
 export RPMDIR=$TOPDIR/rpmbuild
 
-echo "--------------------------------------------------"
-echo "Build OVS RPM from upstream git $TAG"
-echo
-
 mkdir -p $RPMDIR/RPMS
 mkdir -p $RPMDIR/SOURCES
 mkdir -p $RPMDIR/SPECS
@@ -67,15 +70,12 @@ then
 fi
 
 #
-# Build ovs rpm without DPDK from ovs master
-#
-echo =======Build ovs rpm and ovs kmod rpm without DPDK Test in VM==========
-    BuildAndTestOVS.sh -p none -t
-#
 # Build ovs rpm with DPDK
 #
-echo =======Build ovs rpm with DPDK Test in VM==========
-BuildAndTestOVS.sh -d -p none -t
+echo =============================================
+echo =======Build ovs rpm with DPDK and test in VM.
+echo
+BuildAndTestOVS.sh -d -g master -p none -t
 #
 # Build special version of ovs with patches --TODO
 #
