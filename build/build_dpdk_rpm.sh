489db2bdd209e4b2bc53b6199978433b64865b84
Thomas F Herbert <therbert@redhat.com>
2016-02-09 22:17:45 -0500

    Build OVS with dpdk
    Builds dpdk 2.2.0 and Open vSwitch 5.90.
    Includes Multi-Queue and recent patches.
    Open vSwitch RPM is now built with DPDK dataplane.
    Upstream copr (Fedora) spec files and patches are used for building
    
    Also includes fixes of RPM building and command line options
    Adds dpdk arg to RPM test script
    
    Caveat: VM doesn't support sse3 architecture option required by DPDK.
    Workaround test RPM script in host instead of VM.
    
    Install prereqs for dpdk build.
    Verified 'set -e' in all scripts for error termination.
    
    Change-Id: I8d5892ff61a814271972e8d2279628d645b2831b
    Signed-off-by: Thomas F Herbert <therbert@redhat.com>

---
+++ build/build_dpdk_rpm.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+
+# Copyright (c) 2016 Open Platform for NFV Project, Inc. and its contributors
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+set -e
+
+echo "==============================="
+echo executing $0 $@
+echo executing on machine `uname -a`
+
+usage() {
+    echo run BuildAndTestOVS -h for complete help on options on ovsnfv scripts.
+    echo $0 -g version -- only 2.2.0 and master supported
+}
+
+while getopts "g:hp:u:v" opt; do
+    case "$opt" in
+        g)
+            DPDK_VERSION=${OPTARG}
+            ;;
+        h|\?)
+            usage
+            exit 1
+            ;;
+        p)
+            DPDK_PATCH=${OPTARG}
+            ;;
+        u)
+            DPDK_REPO_URL=${OPTARG}
+            ;;
+        v)
+            verbose="yes"
+            ;;
+    esac
+done
+
+if [ -z $DPDK_REPO_URL ]; then
+    DPDK_REPO_URL=http://dpdk.org/git/dpdk
+fi
+if [ -z $DPDK_VERSION ]; then
+    DPDK_VERSION=2.2.0
+fi
+
+HOME=`pwd`
+TOPDIR=$HOME
+TMPDIR=$TOPDIR/rpms
+
+if [ -d $TMPDIR ]
+then
+    rm -rf $TMPDIR
+fi
+
+echo "---------------------"
+echo "Install dependencies for dpdk"
+echo
+sudo yum -y install gcc make python-devel openssl-devel autoconf automake rpm-build \
+            redhat-rpm-config libtool libpcap-devel numactl-devel python-sphinx \
+            libvirt-devel
+
+
+mkdir -p $TMPDIR
+
+cd $TMPDIR
+
+mkdir -p $HOME/rpmbuild/RPMS
+mkdir -p $HOME/rpmbuild/SOURCES
+mkdir -p $HOME/rpmbuild/SPECS
+mkdir -p $HOME/rpmbuild/SRPMS
+
+RPMDIR=$HOME/rpmbuild
+
+#
+# Use Fedora copr spec file
+#
+mkdir -p copr
+cd copr
+git clone http://copr-dist-git.fedorainfracloud.org/cgit/pmatilai/dpdk/dpdk.git
+
+echo "Build DPDK RPM version $DPDK_VERSION"
+echo
+echo "---------------------"
+echo "If building master, clone dpdk repo otherwise fetch snapshot"
+echo "Build DPDK RPM version $DPDK_VERSION"
+echo
+    
+if [[ "$DPDK_VERSION" =~ "master" ]]; then
+    cd $TMPDIR
+    git clone $DPDK_REPO_URL
+    cd dpdk
+    echo "Use local spec file that allows version to be overridden"
+    cp $HOME/dpdk.spec $TMPDIR/dpdk
+    cp $HOME/dpdk.spec $RPMDIR/SPECS
+    cp $TMPDIR/copr/dpdk/dpdk-snapshot.sh $TMPDIR/dpdk
+    cp $TMPDIR/copr/dpdk/*.patch $TMPDIR/copr/dpdk/sources $TMPDIR/copr/dpdk/dpdk-snapshot.sh $RPMDIR/SOURCES
+    snapgit=`git log --pretty=oneline -n1|cut -c1-8`
+    snapser=`git log --pretty=oneline | wc -l`
+
+    makever=`make showversion`
+    basever=`echo ${makever} | cut -d- -f1`
+
+    prefix=dpdk-${basever}-${snapser}.git${snapgit}
+    archive=${prefix}.tar.gz
+    DPDK_VERSION=$basever
+
+    echo "Creating ${archive}"
+    git archive --prefix=${prefix}/ HEAD  | gzip -9 > ${archive}
+    cp ${archive} $RPMDIR/SOURCES
+    echo rpmbuild -bb --define "_topdir $RPMDIR" --define "ver $DPDK_VERSION" --define "snapver ${snapser}.git${snapgit}"
+    rpmbuild -bb --define "_topdir $RPMDIR" --define "ver $DPDK_VERSION" --define "snapver ${snapser}.git${snapgit}" dpdk.spec
+else
+    echo building RPM for DPDK version $DPDK_VERSION
+    echo Get rpm $DPDK_VERSION from upstream
+    wget -q http://dpdk.org/browse/dpdk/snapshot/dpdk-$DPDK_VERSION.tar.gz
+    cp dpdk-$DPDK_VERSION.tar.gz $RPMDIR/SOURCES
+    cp $HOME/dpdk.spec $TMPDIR/copr/dpdk
+    cp $HOME/dpdk.spec $RPMDIR/SPECS
+    cd $TMPDIR/copr/dpdk
+    cp $TMPDIR/copr/dpdk/*.patch $TMPDIR/copr/dpdk/sources $TMPDIR/copr/dpdk/dpdk-snapshot.sh $RPMDIR/SOURCES
+    cp $HOME/dpdk.spec $RPMDIR/SPECS
+    echo rpmbuild -bb --define "_topdir $RPMDIR" --define "ver $DPDK_VERSION" dpdk.spec
+    rpmbuild -bb --define "_topdir $RPMDIR" --define "ver $DPDK_VERSION" dpdk.spec
+fi
+cp $RPMDIR/RPMS/x86_64/*.rpm $HOME
+
+exit 0
--- build/build_ovs_rpm.sh
+++ build/build_ovs_rpm.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-##############################################################################
-# Copyright (c) 2016 Red Hat Inc. and others.
-# therbert@redhat.com
-# All rights reserved. This program and the accompanying materials
-# are made available under the terms of the Apache License, Version 2.0
-# which accompanies this distribution, and is available at
-# http://www.apache.org/licenses/LICENSE-2.0
-##############################################################################
+
+# Copyright (c) 2016 Open Platform for NFV Project, Inc. and its contributors
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
 set -e
-declare -i CNT
 
 echo "==============================="
 echo executing $0 $@
@@ -52,78 +57,137 @@ HOME=`pwd`
 TOPDIR=$HOME
 TMPDIR=$TOPDIR/ovsrpm
 
+echo "---------------------------------------"
+echo Clean out old working dir
+echo
 if [ -d $TMPDIR ]
 then
     rm -rf $TMPDIR
 fi
 
+echo "----------------------------------------"
+echo Install pre-reqs.
+echo
 sudo yum -y install gcc make python-devel openssl-devel kernel-devel graphviz \
        kernel-debug-devel autoconf automake rpm-build redhat-rpm-config \
-       libtool
+       libtool python-twisted-core desktop-file-utils groff
 
 VERSION=2.3.90
 os_type=fedora
 kernel_version=$(uname -a | awk '{print $3}')
 
-mkdir -p $TMPDIR
+RPMDIR=$HOME/rpmbuild
 
-cd $TMPDIR
+echo "---------------------------------------"
+echo Clean out old reminents of old rpms and rpm _topdir.
+echo
 
+rm openvswitch*.rpm || true
+if [  -d $RPMDIR ]; then
+    rm -rf $RPMDIR
+fi
+
+echo "---------------------------------------"
+echo Create new rpm _topdir.
+echo
 mkdir -p $HOME/rpmbuild/RPMS
 mkdir -p $HOME/rpmbuild/SOURCES
 mkdir -p $HOME/rpmbuild/SPECS
 mkdir -p $HOME/rpmbuild/SRPMS
 
-RPMDIR=$HOME/rpmbuild
 
+mkdir -p $TMPDIR
 
-echo "---------------------"
-echo "Clone git repo $OVS_REPO_URL and checkout branch or tag $TAG"
-echo
-git clone $OVS_REPO_URL
+cd $TMPDIR
 
-cd ovs
-echo "--------------------"
-echo "Checkout OVS $TAG"
-echo
-if [[ ! "$TAG" =~ "master" ]]; then
-    git checkout $TAG
-fi
-if [[ ! "$OVS_PATCH" =~ "no" ]]; then
-    echo "Apply patches from $OVS_PATCH"
-fi
-./boot.sh
 if [ ! -z $DPDK ]; then
-    ./configure --with-dpdk
+    echo "----------------------------------"
+    echo "Build OVS for dpdk from Fedora copr repo"
+    echo "Copy patches and other files."
+    echo
+    git clone http://copr-dist-git.fedorainfracloud.org/cgit/pmatilai/dpdk/openvswitch.git
+    cp $TMPDIR/openvswitch/openvswitch.spec $RPMDIR/SPECS
+    cp $TMPDIR/openvswitch/* $RPMDIR/SOURCES
+    echo "-------------------------------------------"
+    echo "Remove old dpdk development rpm"
+    echo
+    sudo rpm -e --allmatches dpdk-devel || true
+    echo "-------------------------------------------"
+    echo "Install dpdk development rpm"
+    echo
+    sudo rpm -ivh $HOME/dpdk-devel*.rpm
+    echo "----------------------------------------"
+    echo "Copy DPDK RPM to SOURCES"
+    echo
+    cp $HOME/*.rpm $RPMDIR/SOURCES
 else
-    ./configure --with-linux=/lib/modules/`uname -r`/build
+    echo "Build OVS RPMs using spec files for $os_type in OVS distribution."
 fi
-echo "--------------------"
-echo "Make OVS $TAG"
-echo
-make
 
-if [[ "$TAG" =~ "master" ]]; then
-    v=$($TMPDIR/ovs/utilities/ovs-vsctl --version | head -1 | cut -d' ' -f4)
-    export VERSION=$v
+if [ -z $DPDK ]; then
+    if [[ "$TAG" =~ "master" ]]; then
+        git clone $OVS_REPO_URL
+        cd ovs
+
+        if [[ ! "$OVS_PATCH" =~ "no" ]]; then
+            echo "Apply patches from $OVS_PATCH"
+        fi
+        basever=`grep AC_INIT configure.ac | cut -d' ' -f2 | cut -d, -f1`
+        export VERSION=$basever
+
+        echo "--------------------------------------------"
+        echo making distribution tarball for Open vswitch version $VERSION
+        echo
+        ./boot.sh
+        ./configure
+        make dist
+
+        echo cp openvswitch-*.tar.gz $HOME/rpmbuild/SOURCES
+        cp openvswitch-*.tar.gz $HOME/rpmbuild/SOURCES
+    else
+        export VERSION=${TAG}
+        echo "---------------------------------------------"
+        echo "Get openvswith-${VERSION}.tar.gz"
+        echo
+        curl --silent --output $HOME/rpmbuild/SOURCES/openvswitch-${VERSION}.tar.gz http://openvswitch.org/releases/openvswitch-${VERSION}.tar.gz
+    fi
+
+    if [ ! -z $kmod ]; then
+        echo "Building kernel module RPM"
+        rpmbuild -bb -D "kversion $kernel_version" -D "kflavors default" --define "_topdir `echo $RPMDIR`" $setnocheck rhel/openvswitch-kmod-${os_type}.spec
+    fi
+    echo " Kernel RPM built!"
+    echo "--------------------------------------------"
+    echo "Building User Space RPM from spec file in OVS"
+    echo
+    rpmbuild -bb --define "_topdir `echo $RPMDIR`" $setnocheck rhel/openvswitch.spec
 else
-    export VERSION=${TAG:1}
+    echo "--------------------------------------------"
+    echo "Building User Space OVS with DPDK RPM with files from Fedora copr"
+    echo
+    if [[ "$TAG" =~ "master" ]]; then
+        cd $TMPDIR
+        git clone $OVS_REPO_URL
+        cd ovs
+        echo "--------------------------------------------"
+        echo "Use ovs-opnfv spec file"
+        cp $HOME/openvswitch.spec $TMPDIR/openvswitch
+        cp $HOME/openvswitch.spec $RPMDIR/SPECS
+        cp $HOME/openvswitch.spec $RPMDIR/SOURCES
+        cp $TMPDIR/openvswitch/openvswitch.spec .
+        snapgit=`git log --pretty=oneline -n1|cut -c1-8`
+        snapser=`git log --pretty=oneline | wc -l`
+        basever=`grep AC_INIT configure.ac | cut -d' ' -f2 | cut -d, -f1`
+        prefix=openvswitch-${basever}-${snapser}.git${snapgit}
+        archive=$prefix.tar.gz
+        echo "--------------------------------------------"
+        echo "Creating archive, $archive"
+        echo
+        git archive --prefix=${prefix}/ HEAD  | gzip -9 > $RPMDIR/SOURCES/${archive}
+    fi
+    cd $TMPDIR/openvswitch
+    rpmbuild -bb --define "ver $basever" --define "snapver $snapser.git$snapgit" --define "_topdir `echo $RPMDIR`" $setnocheck openvswitch.spec
 fi
-
-echo making RPM for Open vswitch version $VERSION
-make dist
-
-echo cp openvswitch-*.tar.gz $HOME/rpmbuild/SOURCES
-cp openvswitch-*.tar.gz $HOME/rpmbuild/SOURCES
-
-if [ ! -z $kmod ]; then
-    echo "Building kernel module..."
-    rpmbuild -bb -D "kversion $kernel_version" -D "kflavors default" --define "_topdir `echo $RPMDIR`" $setnocheck rhel/openvswitch-kmod-${os_type}.spec
-echo " Kernel RPM built!"
-fi
-
-echo "Building User Space..."
-rpmbuild -bb --define "_topdir `echo $RPMDIR`" $setnocheck rhel/openvswitch.spec
 
 cp $RPMDIR/RPMS/x86_64/*.rpm $HOME
 
