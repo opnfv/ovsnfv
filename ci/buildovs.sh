77d42ff63e9209cd1c6bbc567068c4e0382dc71f
Thomas F Herbert <therbert@redhat.com>
2016-06-26 05:53:49 -0400

    Force delete of delorean rpms A few commands to diagnose what is wrong
    Change-Id: I41d008b319318b6f3a64f7e34793a7e2c17cfa2e
    Signed-off-by: Thomas F Herbert <therbert@redhat.com>

--- ci/buildovs.sh
+++ ci/buildovs.sh
@@ -35,7 +35,31 @@ export DATE=`date +%Y-%m-%d`
 
 export BUILD_BASE=$WORKSPACE/build
 
-
+echo ls -lf /etc/yum.repos.d
+sudo ls -lf /etc/yum.repos.d
+echo rm -f /etc/yum.repos.d/delorean.repo
+sudo rm -f /etc/yum.repos.d/delorean.repo
+echo rm -f /etc/yum.repos.d/delorean-deps.repo
+sudo rm -f /etc/yum.repos.d/delorean-deps.repo
+echo yum -y clean all
+sudo yum -y clean all
+echo yum -y update
+sudo yum -y update
+echo yum install epel-release
+sudo yum install epel-release
+echo yum install kernel-headers
+sudo  yum install kernel-headers
+echo yum reinstall kernel-devel
+sudo  yum install kernel-devel
+uname -r
+ls -lai /lib/modules/`uname -r`/build
+ls -lai /usr/src/kernels/`uname -r`
+echo yum install kernel-headers
+sudo  yum install kernel-headers
+echo yum reinstall kernel-devel
+sudo  yum reinstall kernel-devel
+set +e
+exit 0
 
 if [ ! -d $BUILD_BASE ]
 then
